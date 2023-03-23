62153cbcbb9fc3721d79611b76d1a3a34b7577a6
mwlk <mirkoivowlk@gmail.com>
2023-03-22 21:30:42 -0300

    first

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"curso-go/variables"
+)
+
+func main() {
+	variables.GetNumbers()
+}
---
+++ variables/enteras.go
@@ -0,0 +1,13 @@
+package variables
+
+import "fmt"
+
+func GetNumbers() {
+	var comuntint int
+	intde32 := int32(20)
+	intde64 := int64(200)
+
+	fmt.Println("int 1 =", comuntint)
+	fmt.Println("int 2 =", intde32)
+	fmt.Println("int 3 =", intde64)
+}
